5fffc90b67584a768e4d5d1e904eeee655fa4771
Dominic Gerweck <doger83@users.noreply.github.com>
2019-12-29 14:46:34 +0100

    Day 21: initialize g2Asteroids WPF game

---
+++ Projects/g2Asteroids/Classes/Asteroid.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace g2Asteroids
+{
+    class Asteroid : g2AsteroidsObject
+    {
+        static readonly Random zufällig = new Random();
+
+        //Konstruktor der Kindklasse
+        public Asteroid(Canvas zeichenfläche)
+                : base(0.5, 0.5, ( zufällig.NextDouble() - 0.5 ) * 250,
+                                 ( zufällig.NextDouble() - 0.5 ) * 250)
+        {
+            for (int i = 0 ; i < 15 ; i++)
+            {
+                double alpha = 2.0 * Math.PI / 15 * i;
+                double radius = 25.0 + 15.0 * zufällig.NextDouble();
+                umriss.Points.Add(new Point(radius * Math.Cos(alpha),
+                                            radius * Math.Sin(alpha)));
+            }
+            umriss.StrokeThickness = 5;
+            umriss.Stroke = Brushes.LightBlue;
+            umriss.Fill = Brushes.Gray;
+        }
+
+        public override void ZeichneObjekt(Canvas zeichenfläche)
+        {
+            zeichenfläche.Children.Add(umriss);
+            Canvas.SetLeft(umriss, X);
+            Canvas.SetTop(umriss, Y);
+        }
+
+        public bool Collided(double x, double y)
+        {
+            return umriss.RenderedGeometry.FillContains(new Point(x - X, y - Y));
+        }
+    }
+}
---
+++ Projects/g2Asteroids/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+
+namespace g2Asteroids
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //_MainWindow.WindowState = WindowState.Maximized;
+        }
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonStart.IsEnabled = false;
+            ButtonQuit.IsEnabled = false;
+            GamePlayWindow theSpace = new GamePlayWindow();
+            theSpace.Show();
+        }
+
+        private void ButtonQuit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+
+            bool a = false;
+            bool b = false;
+            bool c;
+
+            if (true)
+            {
+
+            }
+        }
+    }
+}
