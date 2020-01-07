5e6955fa93f833d95099eee3e0fbe9ef0d7849aa
Dominic Gerweck <doger83@users.noreply.github.com>
2020-01-07 20:09:01 +0100

    Clean up

--- Projects/cdsacvdg2.ERP/src/g2.ERP.Core/Interfaces/IDomainEventDispatcher.cs
+++
@@ -1,9 +0,0 @@
-﻿using g2.ERP.Core.SharedKernel;
-
-namespace g2.ERP.Core.Interfaces
-{
-    public interface IDomainEventDispatcher
-    {
-        void Dispatch(BaseDomainEvent domainEvent);
-    }
-}
--- Projects/cdsacvdg2.ERP/src/g2.ERP.Core/SharedKernel/BaseDomainEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace g2.ERP.Core.SharedKernel
-{
-    public abstract class BaseDomainEvent
-    {
-        public DateTime DateOccurred { get; protected set; } = DateTime.UtcNow;
-    }
-}
